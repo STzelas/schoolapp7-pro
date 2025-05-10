cf9b77fbf3a551fdf98900e13315134e63ab5de1
Sot. Tzelatidis <stzelati@gmail.com>
2025-05-10 16:08:51 +0300

    feat: teacher model created with lombok annotations and JPA columns

--- src/main/java/gr/aueb/cf/schoolapp/model/Teacher.java
+++ src/main/java/gr/aueb/cf/schoolapp/model/Teacher.java
@@ -1,4 +1,30 @@
 package gr.aueb.cf.schoolapp.model;
 
-public class Teacher {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "teachers")
+public class Teacher extends AbstractEntity implements IdentifiableEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto increment
+    private Long id;
+
+    // Δεν θα βάλω Name γιατί το default είναι όπως
+    // των variables άρα name = "vat" αν το όνομα είναι vat
+    @Column(unique = true, nullable = false)
+    private String vat;
+
+    @Column(nullable = false)
+    private String firstname;
+
+    @Column(nullable = false)
+    private String lastname;
 }
