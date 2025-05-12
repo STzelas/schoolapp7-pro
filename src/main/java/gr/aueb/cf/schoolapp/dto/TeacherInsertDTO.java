ba9813b7389959a92b87a5751d148c4a135433a4
Sot. Tzelatidis <stzelati@gmail.com>
2025-05-12 18:42:24 +0300

    feat: Teacher DTO changed to records

--- src/main/java/gr/aueb/cf/schoolapp/dto/TeacherInsertDTO.java
+++ src/main/java/gr/aueb/cf/schoolapp/dto/TeacherInsertDTO.java
@@ -2,23 +2,26 @@ package gr.aueb.cf.schoolapp.dto;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.*;
+//import lombok.*;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class TeacherInsertDTO {
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
 
+/**
+ * Τα records είναι immutable απλά στην ονομασία δεν έχει getVat αλλα -> vat
+ */
+public record TeacherInsertDTO (
     @NotNull(message = "Το όνομα δεν μπορεί να μην υπάρχει")
     @Size(min = 2, max = 255, message = "Το όνομα πρέπει να είναι μεταξύ 2 και 255 χαρακτήρων")   // bean validation / τα λογικά δεν γίνονται με bean
-    private String firstname;
+    String firstname,
 
     @NotNull(message = "Το επώνυμο δεν μπορεί να μην υπάρχει")
     @Size(min = 2, max = 255, message = "Το επώνυμο πρέπει να είναι μεταξύ 2 και 255 χαρακτήρων")
-    private String lastname;
+    String lastname,
 
     @NotNull(message = "Το ΑΦΜ δεν μπορεί να μην υπάρχει")
     @Size(min = 9, message = "Το ΑΦΜ πρέπει να περιέχει τουλάχιστον 9 ψηφία")
-    private String vat;
-}
+    String vat
+){}
--- src/main/java/gr/aueb/cf/schoolapp/dto/TeacherReadOnlyDTO.java
+++ src/main/java/gr/aueb/cf/schoolapp/dto/TeacherReadOnlyDTO.java
@@ -2,14 +2,14 @@ package gr.aueb.cf.schoolapp.dto;
 
 import lombok.*;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Getter
+//@Setter
 @Builder
-public class TeacherReadOnlyDTO {
-    private Long id;
-    private String vat;
-    private String firstname;
-    private String lastname;
-}
+public record TeacherReadOnlyDTO(
+    Long id,
+    String vat,
+    String firstname,
+    String lastname
+){}
