0213a2f022ef2f431c889c6764bc06173fdc0635
Sot. Tzelatidis <stzelati@gmail.com>
2025-05-16 22:00:54 +0300

    feat: URI build and response return added

--- src/main/java/gr/aueb/cf/schoolapp/rest/TeacherRestController.java
+++ src/main/java/gr/aueb/cf/schoolapp/rest/TeacherRestController.java
@@ -1,18 +1,25 @@
 package gr.aueb.cf.schoolapp.rest;
 
+import gr.aueb.cf.schoolapp.core.exceptions.EntityAlreadyExistsException;
 import gr.aueb.cf.schoolapp.core.exceptions.EntityGenericException;
 import gr.aueb.cf.schoolapp.core.exceptions.EntityInvalidArgumentException;
 import gr.aueb.cf.schoolapp.dto.TeacherInsertDTO;
+import gr.aueb.cf.schoolapp.dto.TeacherReadOnlyDTO;
 import gr.aueb.cf.schoolapp.service.ITeacherService;
 import gr.aueb.cf.schoolapp.validator.ValidatorUtil;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.UriInfo;
 import lombok.RequiredArgsConstructor;
 
+import java.net.URI;
 import java.util.List;
 
 /**
@@ -43,15 +50,36 @@ public class TeacherRestController {
      * Ποιό είναι το URI που έχει κληθεί?
      * Υπάρχει το info μεσα στο UriInfo, κάνουμε inject at runtime
      */
-    public Response addTeacher(TeacherInsertDTO insertDTO, @Context UriInfo uriInfo) throws EntityInvalidArgumentException {
+    @POST
+    @Path("")
+    @Consumes(MediaType.APPLICATION_JSON) // Όταν υπάρχει data request
+    @Produces(MediaType.APPLICATION_JSON) // response
+    public Response addTeacher(TeacherInsertDTO insertDTO, @Context UriInfo uriInfo)
+            throws EntityInvalidArgumentException, EntityAlreadyExistsException {
         List<String> errors = ValidatorUtil.validateDTO(insertDTO);
         if (!errors.isEmpty()) {
             throw new EntityInvalidArgumentException("Teacher", String.join("\n", errors));
         }
 
+        TeacherReadOnlyDTO readOnlyDTO = teacherService.insertTeacher(insertDTO);
 
+        /**
+         * Αυτό που θα επιστρέψουμε μέσα στους headers
+         * Είναι συνένωση του absolute path + id ("/teachers/(id) πχ 1")
+         */
+        URI newResourceUri = uriInfo
+                .getAbsolutePathBuilder()
+                .path(String.valueOf(readOnlyDTO.id()))
+                .build();
+
+        /**
+         * Τρία πράγματα περνάμε στα responses
+         * 1. στοιχεία του header (αν θέλουμε)
+         * 2. HTTP status code,
+         * 3. payload
+         */
+        return Response.created(newResourceUri)
+                .entity(readOnlyDTO)
+                .build();
     }
-
-
-
 }
