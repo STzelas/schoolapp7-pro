cff0e0a507a38d08e569040c7e7d1ef0a74f737e
Sot. Tzelatidis <stzelati@gmail.com>
2025-05-20 11:24:15 +0300

    feat: enums added for user role types

---
+++ src/main/java/gr/aueb/cf/schoolapp/core/enums/RoleType.java
@@ -0,0 +1,9 @@
+package gr.aueb.cf.schoolapp.core.enums;
+
+public enum RoleType {
+    TEACHER,    // 0
+    STUDENT,    // 1
+    ADMIN       // 2
+}
+
+// Για να πάρω το String το αντίστοιχο TEACHER.name()
