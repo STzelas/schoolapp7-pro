7707540bf55ade2e88587527ae0522183071eafe
Sot. Tzelatidis <stzelati@gmail.com>
2025-05-20 11:24:47 +0300

    feat: User model created with lombok and jpa

---
+++ src/main/java/gr/aueb/cf/schoolapp/model/User.java
@@ -0,0 +1,46 @@
+package gr.aueb.cf.schoolapp.model;
+
+import gr.aueb.cf.schoolapp.core.enums.RoleType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.security.auth.Subject;
+import java.io.Serializable;
+import java.security.Principal;
+
+/**
+ * Principal λέμε τον LoggedIn χρήστη.
+ * Το κάνουμε για γίνει ο χρήστης συστηματικός
+ * να μπει μέσα στο σύστημα που έχουμε φτιάκει δηλαδή
+ * πχ το username του ή το αφμ του, κάτι που να
+ * τον μοναδικοποιεί
+ * Κάνουμε implements το principal του java.security
+ */
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements Principal, Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // auto increment
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role")
+    private RoleType roleType;
+
+    @Override
+    public String getName() {
+        return username;
+    }
+}
